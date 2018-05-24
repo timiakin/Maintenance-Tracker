dad59cceb7edd706b2db389b50fb533fd08ee217
timiakin <timi.akin@hotmail.com>
2018-05-24 11:31:21 +0100

    commiting Delete APi and mocha tets

--- API/app.js
+++ API/app.js
@@ -1,22 +1,26 @@
+
+
 const express = require('express');
 
 const bodyparser = require('body-parser');
 
 const app = express();
 
+
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 const requests = [{ id: 0, title: 'Bad Engine' }, { id: 1, title: 'Broken mirror' }, { id: 2, title: 'Fix my tyres' }];
 
+
 app.get('/api/v1/users/requests', (req, res) => { // Fetching all requests
   res.send(requests);
 });
 
-app.get('/api/v1/users/requests/:userID', (req, res) => { // Fetching a particular request
-  if (req.params.userID) {
+app.get('/api/v1/users/requests/:ID', (req, res) => { // Fetching a particular request
+  if (req.params.ID) {
     console.log(req.params);
-    const number = req.params.userID;
+    const number = req.params.ID;
     res.send(`${requests[number].title} ,this is request number:  ${number}`);
   } else {
     res.send('error');
@@ -24,18 +28,34 @@ app.get('/api/v1/users/requests/:userID', (req, res) => { // Fetching a particul
 });
 
 app.post('/api/v1/users/requests', (req, res) => { // Adding a request
-  if (req.body.title && req.body.id) {
+  console.log(req.body.id);
+  const newi = [];
+  for (let x = 0; x < requests.length; x += 1) {
+    newi.push(Number(requests[x].id));
+  }
+  console.log(newi);
+  if (newi.includes(Number(req.body.id))) {
+    res.send('Cannot create new request with the same ID');
+  } else {
+    // console.log(item.id);
     requests.push({ id: req.body.id, title: req.body.title });
     console.log(req.body.title);
     res.send(requests);
-  } else {
-    res.send('Error');
   }
 });
 
-app.put('/api/v1/users/requests/:userID', (req, res) => { // Modifying  a request
-  const number = req.params.userID;
+
+app.put('/api/v1/users/requests/:ID', (req, res) => { // Modifying  a request
+  const number = req.params.ID;
   requests[number].title = req.body.title;
+  console.log(req.params.ID);
+  console.log(req.body.title);
+  res.send(requests);
+});
+
+app.delete('/api/v1/users/requests/delete/:userID', (req, res) => { // Modifying  a request
+  const number = req.params.userID;
+  requests.splice(number, 1);
   console.log(req.params.userID);
   console.log(req.body.title);
   res.send(requests);
@@ -47,5 +67,4 @@ app.listen(port, () => { // starting the server
   console.log(`We are live on ${port}`);
 });
 
-export default app;
-
+module.exports = app;
